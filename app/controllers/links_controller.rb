7dd969472cd3359e3e11312860969eba77bcb510
Michael McDevitt <mmcdevi1@gmail.com>
2015-05-07 16:12:23 -0400

    links controller

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,4 +1,21 @@
 class LinksController < ApplicationController
 	def index
+		@link  = Link.new
+		@links = Link.all
+	end
+
+	def create
+		@link = Link.new(links_params)
+		if @link.save
+			redirect_to root_path
+		else
+			render 'new'
+		end
+	end
+
+	private 
+
+	def links_params
+		params.require(:link).permit(:url)
 	end
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,10 @@
 class Link < ActiveRecord::Base
+	after_create :create_slug
+
+	private
+
+	def create_slug
+		self.slug = self.id.to_s(36)
+		self.save
+	end
 end
