0322a4713a9e2b4116b1e51b77d85a938178cc4b
Michael McDevitt <mmcdevi1@gmail.com>
2015-05-07 16:38:32 -0400

    config routes

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -5,7 +5,7 @@ class LinksController < ApplicationController
 	end
 
 	def show
-		@link = Link.find(params[:id])
+		@link = Link.find_by_slug(params[:id])
 		redirect_to @link.url
 	end
 
@@ -24,3 +24,14 @@ class LinksController < ApplicationController
 		params.require(:link).permit(:url)
 	end
 end
+# def show
+#     if params[:slug]
+#       @link = Link.find_by(slug: params[:slug])
+#       if redirect_to @link.given_url
+#         @link.clicks += 1
+#         @link.save
+#       end
+#     else
+#       @link = Link.find(params[:id])
+#     end
+#   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'links#index'
 
-  resources :links, only: [:create, :show]
+  resources :links, only: [:create]
+
+  get ':id', to: 'links#show', as: :slug
 end
