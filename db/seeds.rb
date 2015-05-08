bea18afb1bb00ae01d2f1dce863d10d508929ac2
Michael McDevitt <mmcdevi1@gmail.com>
2015-05-08 10:13:36 -0400

    seed db file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+150.times do |i|
+	Link.create(url: Faker::Internet.url, clicks: i)
+end
